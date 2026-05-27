1642df06dcd0bdf46bf70c42a115eea6db7d7e51
Your Name <ks218258@gmail.com>
2026-05-27 12:56:44 +0530

    day 3

--- day 2/day3/destructor.cpp
+++ day 2/day3/destructor.cpp
@@ -2,42 +2,53 @@
 using namespace std;
 
 class Resource {
-   private:
+private:
     int* data;
     int id;
-    public:
-    Resource(int i,int val): id(i){
-        data = new int[100]; // Allocate some resource (e.g., an array)
-        cout << "Resource " << id << " acquired(value: " << *data <<")"<< endl;
+
+public:
+    Resource(int i, int val) : id(i) {
+        data = new int[100];
+        data[0] = val;
+
+        cout << "Resource " << id 
+             << " acquired (value: " << data[0] << ")" 
+             << endl;
     }
 
-    Resource() {
-        delete data;
-        cout<<"Resource " << id << " released" << endl;
+    ~Resource() {
+        delete[] data;
+
+        cout << "Resource " << id 
+             << " released" 
+             << endl;
     }
 
     void show() {
-        cout << "Resource " << id << ";"<<*data<< endl;
+        cout << "Resource " 
+             << id << " : " 
+             << data[0] 
+             << endl;
     }
 };
 
 void function() {
-    Resource local(2,200);
+    Resource local(2, 200);
     local.show();
-
 }
 
-int main(){
-    Resource r1(1,100);
+int main() {
+    Resource r1(1, 100);
     r1.show();
 
     function();
 
-    Resource* r3=new Resource(3,300);
+    Resource* r3 = new Resource(3, 300);
     r3->show();
+
     delete r3;
 
-    cout<<"End of main()" << endl;
-    return 0;
+    cout << "End of main()" << endl;
 
+    return 0;
 }

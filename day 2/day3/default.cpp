a1a14d2ee698cb2a14f7e782f79a963bedfb76f4
Your Name <ks218258@gmail.com>
2026-05-27 10:40:40 +0530

    day 3

---
+++ day 2/day3/default.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+
+class Student {
+    public
+        string name;
+        int age;
+
+        Student() {
+            name = "Unknown";
+            age = 0;
+            cout<<"Default constructor called!"<<endl;
+        }
+
+        void display(){
+            cout<<name<<"("<<age<<")"<<endl;
+        }
+}:
+
+int main(){
+    Student s1;gi
+}

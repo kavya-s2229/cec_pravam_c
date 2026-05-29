01c3df92714129ff2b470654b9f4265f2a8b2edf
Your Name <ks218258@gmail.com>
2026-05-29 12:32:01 +0530

    day4

--- day 2/day3/day 4/abstract.cpp
+++ day 2/day3/day 4/abstract.cpp
@@ -1,41 +1,40 @@
 #include<iostream>
 #include<string>
-using namespace std;    
-
-class shape {
+using namespace std;
+class shape{
     protected:
-        string color;
-public:
-    shape(const string& c) : color(c) {}
-    virtual double area() const = 0;
-    virtual void draw() const = 0;
-    string getColor() const { return color; }
-    virtual ~shape() {}
-};
-
-class circle : public shape {
-    private:
-        double radius;
+    string color;
     public:
-        circle(const string& c, double r) : shape(c), radius(r) {}      
-        double area() const override {
-            return 3.14159 * radius * radius;
-        }
-        void draw() const override {
-            cout << "Drawing a " << color << " circle(area:"<<area()<<")"<<endl;
-
-        }
-    };
-     int main() {
-         shape* shapes[2];
-            shapes[0] = new circle("red", 5.0);         
-            shapes[1]=new Rectangle("blue",4.0,6.0);
-            for (int i = 0; i < 2; ++i) {
-                shapes[i]->draw();
-            }
-
-            for (int i = 0; i < 2; ++i) {
-                delete shapes[i];
-            }
-            return 0;
-     }
+    shape(string c):color(c){}
+    virtual double area() const=0;
+    virtual void draw() const=0;
+    string getcolor() const{
+        return color;
+    }
+    virtual ~shape(){};
+};
+class circle:public shape{
+    private:
+    double radius;
+    public:
+    circle(string c,double r):shape(c),radius(r){}
+    double area() const override{
+        return 3.14159*radius*radius;
+    }
+    void draw() const override{
+        cout<<"Drawing a "<<color<<" circle with radius "<<radius<<endl;
+    }
+};
+int main(){
+    shape*shape[2];
+    shape[0]=new circle("red",5.0);
+    shape[1]=new circle("blue",3.0);
+    for(int i=0;i<2;i++){
+        shape[i]->draw();
+        
+    }
+    for(int i=0;i<2;i++){
+        delete shape[i];
+    }
+    return 0;
+}

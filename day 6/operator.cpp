19e35314f69b47f79dd058f8e49b8a095eb206c0
Your Name <ks218258@gmail.com>
2026-06-04 10:14:46 +0530

    day8

--- day 6/operator.cpp
+++ day 6/operator.cpp
@@ -1,106 +1,22 @@
-#include <iostream>
-#include <stdexcept>
+#include<iostream>
 using namespace std;
 
-class Complex {
-private:
-    double real;
-    double imag;
+class Demo {
+    public:
+       void show(int a) { cout << "One int:"<<a<<endl;}
+       void show(int a, int b) { cout << "Two ints:"<<a<<","<<b<<endl; }
 
-public:
-    Complex(double r = 0, double i = 0) : real(r), imag(i) {}
+       void display(int a, int b) {
+         cout << "Displaying:"<<a<<","<< (b ? to_string(b) : "default") <<endl; 
+        }
 
-    // Addition
-    Complex operator+(const Complex& other) const {
-        return Complex(real + other.real, imag + other.imag);
-    }
-
-    // Subtraction
-    Complex operator-(const Complex& other) const {
-        return Complex(real - other.real, imag - other.imag);
-    }
-
-    // Unary minus
-    Complex operator-() const {
-        return Complex(-real, -imag);
-    }
-
-    // Equality
-    bool operator==(const Complex& other) const {
-        return (real == other.real) && (imag == other.imag);
-    }
-
-    // Prefix ++
-    Complex& operator++() {
-        ++real;
-        ++imag;
-        return *this;
-    }
-
-    // Postfix ++
-    Complex operator++(int) {
-        Complex temp = *this;
-        real++;
-        imag++;
-        return temp;
-    }
-
-    // Subscript operator
-    double operator[](int index) const {
-        if (index == 0)
-            return real;
-        if (index == 1)
-            return imag;
-
-        throw out_of_range("Index must be 0 or 1");
-    }
-
-    friend ostream& operator<<(ostream& os, const Complex& c);
-    friend istream& operator>>(istream& is, Complex& c);
 };
 
-// Output operator
-ostream& operator<<(ostream& os, const Complex& c) {
-    os << c.real;
-
-    if (c.imag >= 0)
-        os << " + " << c.imag << "i";
-    else
-        os << " - " << -c.imag << "i";
-
-    return os;
-}
-
-// Input operator
-istream& operator>>(istream& is, Complex& c) {
-    cout << "Enter real part: ";
-    is >> c.real;
-
-    cout << "Enter imaginary part: ";
-    is >> c.imag;
-
-    return is;
-}
-
 int main() {
-    Complex c1(3, 4);
-    Complex c2(1, 2);
-
-    cout << "c1 = " << c1 << endl;
-    cout << "c2 = " << c2 << endl;
-
-    cout << "c1 + c2 = " << (c1 + c2) << endl;
-    cout << "c1 - c2 = " << (c1 - c2) << endl;
-    cout << "-c1 = " << (-c1) << endl;
-
-    cout << "c1 == c2: "
-         << (c1 == c2 ? "true" : "false") << endl;
-
-    cout << "c1++ = " << (c1++) << ", after: " << c1 << endl;
-    cout << "++c1 = " << (++c1) << endl;
-
-    cout << "c1[0] = " << c1[0]
-         << ", c1[1] = " << c1[1] << endl;
-
+    Demo d;
+    d.show(10); 
+    d.show(10, 20); 
+    d.display(10, 20);
+    d.display(10, 20); 
     return 0;
 }
--- day 6/payment.cpp
+++ day 6/payment.cpp
@@ -1,90 +1,85 @@
-#include <iostream>
-#include <string>
-#include <vector>
+#include<iostream>
 using namespace std;
 
-// Abstract base class
-class Payment {
-protected:
-    double amount;
-    string transactionId;
+class Demo {
+    public:
+       void show(int a) { cout << "One int:"<<a<<endl;}
+       void show(int a, int b) { cout << "Two ints:"<<a<<","<<b<<endl; }
 
-public:
-    Payment(double amt, string id)
-        : amount(amt), transactionId(id) {}
+       void display(int a, int b) {
+         cout << "Displaying:"<<a<<","<< (b ? to_string(b) : "default") <<endl; 
+        }
 
-    virtual void process() = 0;          
-    virtual string getType() const = 0;  
-
-    virtual ~Payment() {}
 };
-class CreditCardPayment : public Payment {
-private:
-    string cardNumber;
 
-public:
-    CreditCardPayment(double amt, string id, string card)
-        : Payment(amt, id), cardNumber(card) {}
-
-    void process() override {
-        cout << "Processing Credit Card payment of $" << amount << endl;
-        cout << "Card ending in: "
-             << cardNumber.substr(cardNumber.length() - 4) << endl;
-        cout << "Transaction: "
-             << transactionId << " - APPROVED" << endl;
-    }
-
-    string getType() const override {
-        return "Credit Card";
-    }
-};
- class PayPalPayment : public Payment {
-private: 
-    string email;
-public:
-    PayPalPayment(double amt, string id, string e)
-        : Payment(amt, id), email(e) {}
-
-    void process() override {
-        cout << "Processing PayPal payment of $" << amount << endl;
-        cout << "PayPal account: " << email << endl;
-        cout << "Transaction: " << transactionId << " - APPROVED" << endl;
-    }
-
-    string getType() const override {
-        return "PayPal";
-    }
- };
-class Cryptopayment : public Payment {
-private:
-    string walletAddress;
-public:
-    Cryptopayment(double amt, string id, string wallet)
-        : Payment(amt, id), walletAddress(wallet) {}
-
-    void process() override {
-        cout << "Processing Cryptocurrency payment of $" << amount << endl;
-        cout << "Wallet address: " << walletAddress << endl;
-        cout << "Transaction: " << transactionId << " - APPROVED" << endl;
-    }
-
-    string getType() const override {
-        return "Cryptocurrency";
-    }
-};
 int main() {
-    vector<Payment*> payments;
-    payments.push_back(new CreditCardPayment(100.0, "TXN12345", "1234-5678-9012-3456"));
-    payments.push_back(new PayPalPayment(50.0, "TXN67890", "user@example.com"));
-    payments.push_back(new Cryptopayment(25.0, "TXN11223", "1A2B3C4D5E6F7G8H9I0J"));
+    Demo d;
+    d.show(10); 
+    d.show(10, 20); 
+    d.display(10, 20);
+    d.display(10, 20); 
+    return 0;
+}#include<iostream>
+using namespace std;
 
-    for (Payment* payment : payments) {
-       cout<<"\n---"<<payment->getType()<<" ---"<<endl;
-        payment->process();
-    }
-    for (Payment* payment : payments) {
-        delete payment;
-    }
+class Demo {
+    public:
+       void show(int a) { cout << "One int:"<<a<<endl;}
+       void show(int a, int b) { cout << "Two ints:"<<a<<","<<b<<endl; }
 
+       void display(int a, int b) {
+         cout << "Displaying:"<<a<<","<< (b ? to_string(b) : "default") <<endl; 
+        }
+
+};
+
+int main() {
+    Demo d;
+    d.show(10); 
+    d.show(10, 20); 
+    d.display(10, 20);
+    d.display(10, 20); 
+    return 0;
+}#include<iostream>
+using namespace std;
+
+class Demo {
+    public:
+       void show(int a) { cout << "One int:"<<a<<endl;}
+       void show(int a, int b) { cout << "Two ints:"<<a<<","<<b<<endl; }
+
+       void display(int a, int b) {
+         cout << "Displaying:"<<a<<","<< (b ? to_string(b) : "default") <<endl; 
+        }
+
+};
+
+int main() {
+    Demo d;
+    d.show(10); 
+    d.show(10, 20); 
+    d.display(10, 20);
+    d.display(10, 20); 
+    return 0;
+}#include<iostream>
+using namespace std;
+
+class Demo {
+    public:
+       void show(int a) { cout << "One int:"<<a<<endl;}
+       void show(int a, int b) { cout << "Two ints:"<<a<<","<<b<<endl; }
+
+       void display(int a, int b) {
+         cout << "Displaying:"<<a<<","<< (b ? to_string(b) : "default") <<endl; 
+        }
+
+};
+
+int main() {
+    Demo d;
+    d.show(10); 
+    d.show(10, 20); 
+    d.display(10, 20);
+    d.display(10, 20); 
     return 0;
 }
